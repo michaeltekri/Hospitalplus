88f6fc8c78debc9a85dbc9586a0b6d0971096d24
franky-collab <waruif94@gmail.com>
2025-03-13 11:27:39 +0300

    new commit

--- hospitalapp/urls.py
+++ hospitalapp/urls.py
@@ -21,4 +21,10 @@ urlpatterns = [
     path('', views.register,name='register'),
     path('login/', views.login_view,name='login'),
 
+#mpesaAPI
+    path('pay/', views.pay, name='pay'),
+    path('stk/', views.stk, name='stk'),
+    path('token/', views.token, name='token'),
+    path('transactions/', views.transactions_list, name='transactions'),
+
 ]
